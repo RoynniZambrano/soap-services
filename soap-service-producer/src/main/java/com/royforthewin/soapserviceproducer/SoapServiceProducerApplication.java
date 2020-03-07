d195a4270c0d71dc69ac71a9c2dd258ee85f62a2
Roy <roynny51@gmail.com>
2020-03-06 22:39:54 -0300

    first commit

---
+++ soap-service-producer/src/main/java/com/royforthewin/soapserviceproducer/SoapServiceProducerApplication.java
@@ -0,0 +1,13 @@
+package com.royforthewin.soapserviceproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapServiceProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapServiceProducerApplication.class, args);
+	}
+
+}
---
+++ soap-service-producer/src/test/java/com/royforthewin/soapserviceproducer/SoapServiceProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.royforthewin.soapserviceproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapServiceProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
