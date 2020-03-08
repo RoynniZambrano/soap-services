e22ecbe2f805e898921ed2cd71c910fddc7265e2
Roy <roynny51@gmail.com>
2020-03-07 22:46:39 -0300

    end module

--- soap-service-consumer/src/main/java/com/royforthewin/soapserviceconsumer/SoapServiceConsumerApplication.java
+++ soap-service-consumer/src/main/java/com/royforthewin/soapserviceconsumer/SoapServiceConsumerApplication.java
@@ -1,13 +1,30 @@
 package com.royforthewin.soapserviceconsumer;
 
+import com.royforthewin.consumingwebservice.wsdl.GetCountryResponse;
+import com.royforthewin.soapserviceconsumer.services.CountryClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SoapServiceConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoapServiceConsumerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner lookup(CountryClient quoteClient) {
+		return args -> {
+			String country = "Spain";
+
+			if (args.length > 0) {
+				country = args[0];
+			}
+			GetCountryResponse response = quoteClient.getCountry(country);
+			System.err.println(response.getCountry().getCurrency());
+		};
 	}
 
 }
---
+++ soap-service-consumer/src/main/java/com/royforthewin/soapserviceconsumer/services/CountryConfiguration.java
@@ -0,0 +1,27 @@
+package com.royforthewin.soapserviceconsumer.services;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class CountryConfiguration {
+
+    @Bean
+    public Jaxb2Marshaller marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        // this package must match the package in the <generatePackage> specified in
+        // pom.xml
+        marshaller.setContextPath("com.royforthewin.consumingwebservice.wsdl");
+        return marshaller;
+    }
+
+    @Bean
+    public CountryClient countryClient(Jaxb2Marshaller marshaller) {
+        CountryClient client = new CountryClient();
+        client.setDefaultUri("http://localhost:8080/ws");
+        client.setMarshaller(marshaller);
+        client.setUnmarshaller(marshaller);
+        return client;
+    }
+}
